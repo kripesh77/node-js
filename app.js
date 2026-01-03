8243bea61e7ca6c88c21a0677e664ed19aa60c7a
kripesh77 <kg9773092+github@gmail.com>
2026-01-03 08:57:48 +0545

    minor refactoring of codes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('query parser', 'extended');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
@@ -36,17 +37,10 @@ app.use('/api/v1/users', userRouter);
 // we do this
 app.use((req, res, next) => {
   // creating an error
-  const err = new AppError(`can't find ${req.originalUrl}`, 404);
-  next(err);
+  next(new AppError(`can't find ${req.originalUrl}`, 404));
 });
 
 // global error handler
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  err.message = err.message || 'Internal Server Error !';
-
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,7 @@
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  err.message = err.message || 'Internal Server Error !';
+
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
+};
