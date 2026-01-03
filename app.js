9258ecaf861108d86cf6e3df8570d5a18d761b1d
kripesh77 <kg9773092+github@gmail.com>
2026-01-03 08:40:12 +0545

    global error handling

--- app.js
+++ app.js
@@ -34,9 +34,23 @@ app.use('/api/v1/users', userRouter);
 
 // we do this
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({ status: 'fail', message: `can't find ${req.originalUrl}` });
+  // creating an error
+  // Now we can go all over the app and define error like this and pass it to next(err);
+  // But there's a better approach of throwing an error, by making a separate error class
+  // refer next commit for details
+  const err = new Error(`can't find ${req.originalUrl}`);
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err);
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  err.message = err.message || 'Internal Server Error !';
+
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
 });
 
 module.exports = app;
