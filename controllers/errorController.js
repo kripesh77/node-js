b2238bc8ff8e24036d7671da4b573e548a1c9488
kripesh77 <kg9773092+github@gmail.com>
2026-01-03 10:15:43 +0545

    Developmental vs Production error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,40 @@
+const sendErrorDev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational: send response to the client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak error details to the client
+  } else {
+    // 1) log the error:
+    console.error('ERROR 💥', err);
+
+    // 2) Send the generic message:
+    return res
+      .status(500)
+      .json({ status: 'error', message: 'Something went very wrong!' });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
+  // In production, we want to leak as little information to the client as possible
+  // while in development, we want as detailed error information as possible
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
