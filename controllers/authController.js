d87df309a52655655e611dc665e9640dbd0baa6d
kripesh77 <kg9773092+github@gmail.com>
2026-01-04 19:06:34 +0545

    JWT in the house

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,23 @@
 // -creating new users, logging users in, or updating password in authentication controllers
 // -All these stuffs related to the authentication will be in authentication controllers rather than user controllers
 
+const jwt = require('jsonwebtoken');
 const UserModel = require('../models/userModel');
 const catchAsyncError = require('../utils/catchAsyncError');
 
 exports.signup = catchAsyncError(async (req, res, next) => {
-  const newUser = await UserModel.create(req.body);
-  return res.status(201).json({ status: 'success', data: { user: newUser } });
+  // one can send role="admin" or similar in req.body, so we donot accept whole req.body directly
+  const newUser = await UserModel.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  return res
+    .status(201)
+    .json({ status: 'success', token, data: { user: newUser } });
 });
