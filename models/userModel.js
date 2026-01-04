2085b7bb14864cd3b4e81bc508e6febc77ad2e99
kripesh77 <kg9773092+github@gmail.com>
2026-01-04 17:20:58 +0545

    AUTHENTICATION AND AUTHORIZATION STARTS FROM HERE. 1) created a basic user schema

---
+++ models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A name is required'],
+    minlength: [3, 'A name should be atleast 3 character long'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is a must'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    validate: {
+      validator: function (val) {
+        return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+          val,
+        );
+      },
+      message:
+        'Password must be at least 8 characters long and include a letter, a number, and a special character',
+    },
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validate: function (val) {
+        return this.password === val;
+      },
+      message: 'Passwords do not match!',
+    },
+  },
+});
+
+const UserModel = mongoose.model('User', userSchema);
+module.exports = UserModel;
