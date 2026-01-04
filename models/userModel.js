3218b76c4985c5bd27c9b62537e77adb0e81b287
kripesh77 <kg9773092+github@gmail.com>
2026-01-04 18:35:53 +0545

    hashed password in pre document middleware before saving, only hashing password if user explicitely asked for,  and found out next() in pre document is deprecated in mongoose 9.x

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,12 +33,32 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your password'],
     validate: {
+      // this only works on CREATE and SAVE!!!
+      // so for this reason, when we are updating the password
+      // we've to use .save() as well and not findOneAndUpdate() or updateOne()
+      // E.g:
+      /* 
+        Always update passwords via .save() in controller
+          const user = await User.findById(id);
+          user.password = newPassword;
+          user.passwordConfirm = confirmPassword;
+          await user.save();
+       */
       validator: function (val) {
         return this.password === val;
       },
       message: 'Passwords do not match!',
     },
   },
+});
+
+// JUST DISCOVERED, next() in pre document middleware like below is deprecated from mongoose 9.x
+userSchema.pre('save', async function () {
+  // we want to hash the password only if new user is being created or user is actually updating the password
+  // Not when other fields are being updated
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
 });
 
 const UserModel = mongoose.model('User', userSchema);
